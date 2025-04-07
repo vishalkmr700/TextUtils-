728d4d71c69d15952cb0f671dfb5451e4a94f464
Vishal Kumar <imvishal8272@gmail.com>
2025-04-07 18:02:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import "./App.css";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      
+      {/* <Navbar title="TextUtils" aboutText="About Us" /> */}
+      <Navbar/>
+    
+    </>
+  );
+}
+
+export default App;
